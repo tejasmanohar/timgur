e473d156c3da860cc5613031eabbcb93b52f9660
Tejas Manohar <me@tejasmanohar.com>
2014-12-19 12:10:36 -0600

    organization of code

--- app.rb
+++ app.rb
@@ -1,16 +1,20 @@
+### Gems
 require 'imgur'
 require 'orchestrate'
 require 'sinatra'
 
+### Environments
 configure :development do
   require 'pry'
   require 'sinatra/reloader'
 end
 
-get '/' do
-  erb :home
-end
-
+### API Clients
 app = Orchestrate::Application.new(ENV['API_KEY'])
 users = app[:users]
 client = Orchestrate::Client.new(ENV['API_KEY'])
+
+### Routes
+get '/' do
+  erb :home
+end
