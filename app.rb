2801b43e87f4b5a26167198db6466eda438a349d
Tejas Manohar <me@tejasmanohar.com>
2014-12-21 04:09:45 -0600

    extended regex, helper method

--- app.rb
+++ app.rb
@@ -14,6 +14,23 @@ app = Orchestrate::Application.new(ENV['API_KEY'])
 subscribers = app[:subscribers]
 client = Orchestrate::Client.new(ENV['API_KEY'])
 
+### Helper Methods
+helpers do
+  def formatted_number(number)
+    digits = number.gsub(/\D/, '').split(//)
+
+    if (digits.length == 11 and digits[0] == '1')
+      # Strip leading 1
+      digits.shift
+    end
+
+    if (digits.length == 10)
+      digits = digits.join
+      '(%s) %s-%s' % [ digits[0,3], digits[3,3], digits[6,4] ]
+    end
+  end
+end
+
 ### Routes
 get '/' do
   status 200
@@ -21,13 +38,12 @@ get '/' do
 end
 
 post '/subscribe' do
-  'request went through'
-  # num = params[:number]
-  # if subscribers[num].empty?
-  #   status 200
-  #   subscribers.create(num, {})
-  # else
-  #   status 400
-  #   'already subscribed'
-  # end
+  num = params[:number]
+  if subscribers[num].empty?
+    status 200
+    subscribers.create(num, {})
+  else
+    status 400
+    'already subscribed'
+  end
 end
